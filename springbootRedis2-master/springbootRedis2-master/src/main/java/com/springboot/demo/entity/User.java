fa3c58ecfcf397c352e63185597ced00ecd88ce5
LAPTOP-4BLPP9LC\ThinkPad <vip163lcl@163.com>
2019-09-26 14:08:21 +0800

    redis

---
+++ springbootRedis2-master/springbootRedis2-master/src/main/java/com/springboot/demo/entity/User.java
@@ -0,0 +1,44 @@
+package com.springboot.demo.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * @ClassName: User
+ * @Auther: zhangyingqi
+ * @Date: 2018/8/27 17:17
+ * @Description:
+ */
+@Entity
+@Table(name = "user")
+@Data
+public class User extends BaseEntity implements Serializable {
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+    
+    
+    
+}
